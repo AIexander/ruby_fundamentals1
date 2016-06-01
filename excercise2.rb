a7147db3caf6fbf635d49b03f1b5f0906fb7eca6
Christian <chrizx0@gmail.com>
2016-06-01 12:17:35 -0400

    refined code to loop without having to re-init.

--- excercise2.rb
+++ excercise2.rb
@@ -1,9 +1,10 @@
-puts "What does the meal cost?"
+loop do
+ puts "What does the meal cost?"
 
 number = gets.to_f
 if number <= 0
   puts "that isnt an acceptable response"
 else
 puts "a good tip on a #{number} dollar meal would be #{(number*0.2).round(2)}$"
-
+end
 end
